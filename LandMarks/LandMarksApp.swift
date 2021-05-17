1753dfb65dc9dd6c93dafee73d9999dddec6341a
nadya <kazynkina@keysystems.ru>
2021-05-17 14:46:16 +0300

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Nadya K on 14.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandMarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by Nadya K on 14.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+            LandmarkList()
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
